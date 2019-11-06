ccbe1e757b6297d062e51bc8a3e5ed53b69ad774
Eric Ke <26385966+ericke8@users.noreply.github.com>
2019-11-06 15:42:56 -0800

    style

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   Route,
-  Link,
   Switch,
 } from 'react-router-dom';
 
@@ -11,10 +10,12 @@ import About from '../components/About';
 class App extends React.Component {
   render() {
     return (
-        <div>
+      <div>
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
-        </div>
+        </Switch>
+      </div>
     );
   }
 };
